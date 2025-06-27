3f22c9aafc2e59f6963d5ae59eb0e2a2e2c5637e
cfcMalky <73409821+cfcMalky@users.noreply.github.com>
2025-06-27 03:09:55 +0100

    Add files via upload

---
+++ inject-banner.js
@@ -0,0 +1,8 @@
+fetch('banner.html')
+  .then(response => response.text())
+  .then(html => {
+    const body = document.body;
+    if (body) {
+      body.insertAdjacentHTML('afterbegin', html);
+    }
+  }); 
---
+++ inject-navbar.js
@@ -0,0 +1,10 @@
+fetch('navbar.html')
+  .then(response => response.text())
+  .then(html => {
+    const banner = document.querySelector('.site-banner');
+    if (banner) {
+      const tempDiv = document.createElement('div');
+      tempDiv.innerHTML = html;
+      banner.insertAdjacentElement('afterend', tempDiv.firstElementChild);
+    }
+  }); 
