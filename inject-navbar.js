b197d7d446ffceeba29fae90d986f3e4297b1c6e
malkymccallum6 <malky.mccallum6@gmail.com>
2025-06-27 11:28:39 +0100

    Update inject-navbar.js

--- inject-navbar.js
+++ inject-navbar.js
@@ -1,10 +1,15 @@
-fetch('/navbar.html')
-  .then(response => response.text())
-  .then(html => {
-    const banner = document.querySelector('.site-banner');
-    if (banner) {
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('/navbar.html')
+    .then(response => response.text())
+    .then(html => {
+      let banner = document.querySelector('.site-banner');
       const tempDiv = document.createElement('div');
       tempDiv.innerHTML = html;
-      banner.insertAdjacentElement('afterend', tempDiv.firstElementChild);
-    }
-  }); 
+      const navbar = tempDiv.firstElementChild;
+      if (banner) {
+        banner.insertAdjacentElement('afterend', navbar);
+      } else {
+        document.body.insertBefore(navbar, document.body.firstChild);
+      }
+    });
+}); 
